12dd9dbc8da8a2e7957d681bf97ae29ed8aba988
israel.hilary7@yahoo.com <honorableisrael>
2021-07-25 14:54:49 +0100

    added updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.edec50fa86fdf949ae2631aa07718c19.js"
+  "/precache-manifest.0e78fdc30cf6efc5bdc95bedf304d18d.js"
 );
 
 self.addEventListener('message', (event) => {
